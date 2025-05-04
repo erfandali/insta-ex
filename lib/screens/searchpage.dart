af3dde50a6963c32b875a9492db28a850cfce810
erfandali <erfandali99@gmail.com>
2025-05-05 02:36:08 +0430

    3up

--- lib/screens/searchpage.dart
+++ lib/screens/searchpage.dart
@@ -1,7 +1,8 @@
+import 'package:expllor/core/tallVideo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:shimmer/shimmer.dart';
-import '../core/tallVideo.dart'; // ویجت مخصوص ویدیوی بلنده
+import 'package:video_player/video_player.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -12,54 +13,43 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   final List<Map<String, dynamic>> rawPosts = [
-    //up
-    {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-    {'type': 'image', 'url': 'https://picsum.photos/201'},
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-    {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-    {'type': 'image', 'url': 'https://picsum.photos/203'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-     {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-      {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-       {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-     {'type': 'image', 'url': 'https://picsum.photos/203'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-     {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-      {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-          {'type': 'image', 'url': 'https://picsum.photos/204'},
-              {'type': 'image', 'url': 'https://picsum.photos/204'},
-       {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-                {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-                       {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},     
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-    {'type': 'video', 'url': 'https://v.ftcdn.net/11/21/46/21/240_F_1121462188_3P2cGXzNE4ZLKOvrYaB2MZAGHXXhycP2_ST.mp4'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'image', 'url': 'https://picsum.photos/204'},
-    {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-        {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-            {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
-                {'type': 'video', 'url': 'https://videos.pexels.com/video-files/31387378/13392869_1080_1920_60fps.mp4'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/200/300'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/250/400'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/300/200'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/350/250'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/400/300'},    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/200/300'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'},
+        {'type': 'image', 'url': 'https://picsum.photos/300/200'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/350/250'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/400/300'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'},
+        {'type': 'image', 'url': 'https://picsum.photos/300/200'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/350/250'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/400/300'},
+    {'type': 'image', 'url': 'https://picsum.photos/250/400'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/300/200'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/350/250'},
+    {'type': 'video', 'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'},
+    {'type': 'image', 'url': 'https://picsum.photos/400/300'},
+
+
   ];
 
   final List<Map<String, dynamic>> arrangedPosts = [];
-  final List<int> tallPositions = [];
+  int _currentTallColumn = 0; // 0=left, 2=right
+  bool _isLoading = true;
 
   @override
   void initState() {
@@ -67,89 +57,124 @@ class _SearchScreenState extends State<SearchScreen> {
     _prepareLayout();
   }
 
-  void _prepareLayout() {
-    List<Map<String, dynamic>> videos = [];
-    List<Map<String, dynamic>> others = [];
+  Future<void> _prepareLayout() async {
+    try {
+      final videos = rawPosts.where((p) => p['type'] == 'video').toList();
+      final images = rawPosts.where((p) => p['type'] == 'image').toList();
 
-    for (var post in rawPosts) {
-      if (post['type'] == 'video') {
-        videos.add(post);
-      } else {
-        others.add(post);
-      }
-    }
+      int videoIndex = 0;
+      int imageIndex = 0;
+      int positionCounter = 0;
 
-    int videoCounter = 0;
-    bool placeLeft = true; // اول بلند چپ قرار بگیره
-
-    while (videos.isNotEmpty || others.isNotEmpty) {
-      if (videos.isNotEmpty) {
-        var video = videos.removeAt(0);
-        bool isTall = (videoCounter % 5 == 0);
-
-        if (isTall) {
-          // اگه Tall بود:
-          if (placeLeft) {
-            arrangedPosts.add(video..['isTall'] = true);
-            tallPositions.add(arrangedPosts.length - 1);
-            for (int i = 0; i < 4 && others.isNotEmpty; i++) {
-              arrangedPosts.add(others.removeAt(0));
+      while (videoIndex < videos.length || imageIndex < images.length) {
+        if (videoIndex < videos.length && positionCounter % 5 == 0) {
+          // Add tall video
+          arrangedPosts.add({
+            ...videos[videoIndex],
+            'isTall': true,
+            'column': _currentTallColumn,
+          });
+          videoIndex++;
+          
+          // Alternate columns
+          _currentTallColumn = _currentTallColumn == 0 ? 2 : 0;
+          
+          // Add 4 regular items
+          for (int i = 0; i < 4; i++) {
+            if (imageIndex < images.length) {
+              arrangedPosts.add({
+                ...images[imageIndex],
+                'isTall': false,
+                'column': _getOppositeColumn(_currentTallColumn, i),
+              });
+              imageIndex++;
+            } else if (videoIndex < videos.length) {
+              arrangedPosts.add({
+                ...videos[videoIndex],
+                'isTall': false,
+                'column': _getOppositeColumn(_currentTallColumn, i),
+              });
+              videoIndex++;
             }
-          } else {
-            for (int i = 0; i < 4 && others.isNotEmpty; i++) {
-              arrangedPosts.add(others.removeAt(0));
-            }
-            arrangedPosts.add(video..['isTall'] = true);
-            tallPositions.add(arrangedPosts.length - 1);
           }
-          placeLeft = !placeLeft;
         } else {
-          arrangedPosts.add(video..['isTall'] = false);
+          // Add regular item
+          if (imageIndex < images.length) {
+            arrangedPosts.add({
+              ...images[imageIndex],
+              'isTall': false,
+              'column': -1, // Auto-position
+            });
+            imageIndex++;
+          } else if (videoIndex < videos.length) {
+            arrangedPosts.add({
+              ...videos[videoIndex],
+              'isTall': false,
+              'column': -1,
+            });
+            videoIndex++;
+          }
         }
-
-        videoCounter++;
-      } else {
-        arrangedPosts.addAll(others);
-        others.clear();
+        positionCounter++;
       }
+    } catch (e) {
+      debugPrint('Layout preparation error: $e');
+    } finally {
+      setState(() => _isLoading = false);
     }
+  }
+
+  int _getOppositeColumn(int tallColumn, int index) {
+    return tallColumn == 0 ? [1, 2][index % 2] : [0, 1][index % 2];
   }
 
   Widget _buildMediaWidget(int index) {
     final post = arrangedPosts[index];
-    final bool isTall = post['isTall'] == true;
-
+    
     if (post['type'] == 'image') {
-      return Stack(
-        fit: StackFit.expand,
-        children: [
-          Image.network(post['url'], fit: BoxFit.cover),
-          const Positioned(
-            bottom: 4,
-            right: 4,
-            child: Icon(Icons.photo, size: 16, color: Colors.white),
-          ),
-        ],
-      );
+      return _buildImageWidget(post);
     }
+    
+    return post['isTall'] 
+      ? TallVideo(post: post)
+      : _buildVideoPlaceholder();
+  }
 
-    if (!isTall) {
-      return Stack(
-        fit: StackFit.expand,
-        children: [
-          Shimmer.fromColors(
-            baseColor: Colors.grey[300]!,
-            highlightColor: Colors.grey[100]!,
-            child: Container(color: Colors.grey[300]),
+  Widget _buildImageWidget(Map<String, dynamic> post) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Image.network(
+          post['url'],
+          fit: BoxFit.cover,
+          errorBuilder: (_, __, ___) => Container(
+            color: Colors.grey[300],
+            child: const Icon(Icons.broken_image, color: Colors.white),
           ),
-          const Center(
-            child: Icon(Icons.play_circle_fill, size: 40, color: Colors.white70),
-          ),
-        ],
-      );
-    }
+        ),
+        const Positioned(
+          bottom: 4,
+          right: 4,
+          child: Icon(Icons.photo, size: 16, color: Colors.white),
+        ),
+      ],
+    );
+  }
 
-    return TallVideo(post: post);
+  Widget _buildVideoPlaceholder() {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[100]!,
+          child: Container(color: Colors.grey[300]),
+        ),
+        const Center(
+          child: Icon(Icons.play_circle_fill, size: 40, color: Colors.white70),
+        ),
+      ],
+    );
   }
 
   @override
@@ -164,48 +189,70 @@ class _SearchScreenState extends State<SearchScreen> {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(11),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  hintStyle: TextStyle(color: Colors.grey),
-                  prefixIcon: Icon(Icons.search, color: Colors.grey),
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(vertical: 12),
-                ),
-              ),
-            ),
-          ),
+          _buildSearchBar(),
           Expanded(
-            child: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: StaggeredGrid.count(
-                crossAxisCount: 3,
-                mainAxisSpacing: 2,
-                crossAxisSpacing: 2,
-                children: List.generate(arrangedPosts.length, (index) {
-                  final isTall = tallPositions.contains(index);
-
-                  return StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: isTall ? 2 : 1,
-                    child: Container(
-                      color: Colors.grey[300],
-                      child: _buildMediaWidget(index),
-                    ),
-                  );
-                }),
-              ),
-            ),
+            child: _isLoading
+              ? _buildLoadingIndicator()
+              : _buildContentGrid(),
           ),
         ],
       ),
     );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(11),
+        ),
+        child: const TextField(
+          decoration: InputDecoration(
+            hintText: 'Search',
+            hintStyle: TextStyle(color: Colors.grey),
+            prefixIcon: Icon(Icons.search, color: Colors.grey),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 12),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildContentGrid() {
+    return  Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: StaggeredGrid.count(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 2,
+                    crossAxisSpacing: 2,
+                    children: [
+                      for (int i = 0; i < arrangedPosts.length; i++)
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: arrangedPosts[i]['isTall'] ? 2 : 1,
+                          child: Container(
+                            color: Colors.grey[300],
+                            child: _buildMediaWidget(i),
+                          ),
+                        ),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+
   }
 }
